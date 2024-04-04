89697d6ca1429a66f6418ceacf78068f9f19a981
Fulbert gato junior Adanhodou <gatojunior.co@gmail.com>
2024-04-04 16:57:12 +0000

    add wallet dto

---
+++ src/main/java/co/afrikreator/afrikreatorcore/dtos/wallet/WalletDTO.java
@@ -0,0 +1,19 @@
+package co.afrikreator.afrikreatorcore.dtos.wallet;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class WalletDTO {
+
+    private Float availableBalance;
+    private Float outcomeBalance;
+    private Float incomeBalance;
+    private Float lastBalance;
+    private String ownerMatricule;
+    private String createdDate;
+    private String lastModifiedDate;
+}
