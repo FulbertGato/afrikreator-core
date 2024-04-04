be2f33c23930b9b0faf512456629962cda40efd8
Fulbert gato junior Adanhodou <gatojunior.co@gmail.com>
2024-04-04 14:42:42 +0000

    first commit

---
+++ src/main/java/co/afrikreator/afrikreatorcore/AfrikreatorCoreApplication.java
@@ -0,0 +1,13 @@
+package co.afrikreator.afrikreatorcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfrikreatorCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfrikreatorCoreApplication.class, args);
+    }
+
+}
